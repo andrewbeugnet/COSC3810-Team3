9e0f50d2c32c0182a2fee70ba896813952f22c30
Sierra Thomas <sierra0926@gmail.com>
2022-03-22 11:10:38 -0500

    update carousel errors

--- weather-station/src/App.js
+++ weather-station/src/App.js
@@ -1,9 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Layouts from './Layout'
-import Splitter from 'm-react-splitters';
 import 'm-react-splitters/lib/splitters.css';
-import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 
 function App() {
