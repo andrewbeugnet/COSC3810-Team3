a5e4b3cd5e092daaaeb7de8848b5f13605f51f70
andrewbeugnet <beugnet.andrew@gmail.com>
2022-03-30 19:06:20 -0500

    Update WeatherStation.cs
    Delete commented lines

--- weather-station-backend/weather-station2/weather-station2/models/WeatherStation.cs
+++ weather-station-backend/weather-station2/weather-station2/models/WeatherStation.cs
@@ -12,33 +12,4 @@ namespace weather_station2.models
 
         public Guid stationId => Guid.NewGuid();
     }
-    // public class WeatherStation
-    // {
-    //     private Thermometer thermometer;
-    //     private Barometer barometer;
-    //     private RainGauge rainGauge;
-    //     private Anemometer anemometer;
-    //     private Guid stationId;
-
-    //     public WeatherStation(double medianTemperature, double medianPressure,  double medianRainfall, double medianWindSpeed)
-    //     {
-    //         thermometer = new Thermometer(medianTemperature);
-    //         barometer = new Barometer(medianPressure);
-    //         rainGauge = new RainGauge(medianRainfall);
-    //         anemometer = new Anemometer(medianWindSpeed);
-
-    //         stationId = Guid.NewGuid();
-    //     }
-
-    //     public WeatherData GetWeatherData()
-    //     {
-    //         return new WeatherData() { 
-    //             Temperature = thermometer.GetTemperature(),
-    //             Pressure = barometer.GetPressure(),
-    //             Rainfall = rainGauge.GetRainfall(),
-    //             Windspeed = anemometer.GetWindSpeed(),
-    //             WindDirection = anemometer.GetWindDirection(),
-    //             Timestamp = DateTime.Now};
-    //     }
-    // }
 }
